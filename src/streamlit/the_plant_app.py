31503920a40c84c02b2d87ef6c6179d707f02c7b
unknown <mpv.flechas10@gmail.com>
2024-11-02 09:29:08 +0100

    Update prediction's probability shown.

--- src/streamlit/the_plant_app.py
+++ src/streamlit/the_plant_app.py
@@ -192,7 +192,8 @@ if page==pages[2]:
             predicted_class_indices=np.argmax(pred,axis=1)
             print("Class index:",predicted_class_indices[0])
             the_class= labels.iloc[predicted_class_indices[0]][0]
-            st.write("Prédictions : "+the_class)
+            probaWeb = np.round(pred[0][predicted_class_indices[0]]*100,1)
+            st.write("Prédictions : "+the_class+" avec "+ str(probaWeb)+" de probabilité.")
             
      # 2. Upload d'une image
     elif option == "Upload d'une image":
@@ -253,7 +254,8 @@ if page==pages[2]:
                 predicted_class_indices=np.argmax(pred,axis=1)
                 print("Class index:",predicted_class_indices[0])
                 the_class= labels.iloc[predicted_class_indices[0]][0]
-                st.write("Prédictions : "+the_class)
+                probaExtract = np.round(pred[0][predicted_class_indices[0]]*100,1)
+                st.write("Prédictions : "+the_class+" avec "+ str(probaExtract)+" de probabilité.")
                 st.image(img, caption="Image extraite depuis l'URL")
             except Exception as e:
                 st.error(f"Erreur lors de l'extraction de l'image : {e}")
@@ -314,7 +316,8 @@ if page==pages[2]:
                 predicted_class_indices=np.argmax(pred,axis=1)
                 print("Class index:",predicted_class_indices[0])
                 the_class= labels.iloc[predicted_class_indices[0]][0]
-                st.write("Prédictions : "+the_class)    
+                probaRepo = np.round(pred[0][predicted_class_indices[0]]*100,1)
+                st.write("Prédictions : "+the_class+" avec "+ str(probaRepo)+" de probabilité.")  
                 st.image(image, caption=f"Exemple d'image : {example_choice}")        
             except FileNotFoundError:
                 st.error(f"Erreur : L'image '{example_choice}' n'a pas été trouvée dans le répertoire.")
